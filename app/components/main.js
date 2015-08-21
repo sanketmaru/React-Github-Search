9f20cd183192ba1e9c634ac482051cfaefc1adf6
sanketmaru <sanketmaru@gmail.com>
2015-08-21 16:31:45 +0530

    main component changes

---
+++ app/components/main.js
@@ -0,0 +1,28 @@
+var React = require('react');
+var RouteHandler = require('react-router').RouteHandler;
+
+var App = React.createClass({
+  
+  clickHandler: function(){
+    console.log(this.props.msg);
+  },
+  
+  render:function(){
+  	return (
+      <div className="main-container">
+        <nav className="navbar navbar-default" role="navigation">
+          <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
+            Menu
+          </div>
+        </nav>
+    		<div className="container">
+  	    	<RouteHandler />
+  	    </div>
+      </div>
+  	)
+    
+  }
+
+});
+
+module.exports = App;
--- app/config/routes.js
+++ app/config/routes.js
@@ -1,11 +1,13 @@
 var React = require('react');
 var Home = require('../components/home');
+var Main = require('../components/main');
 var Router = require('react-router');
 var DefaultRoute = Router.DefaultRoute;
 var Route =  Router.Route;
 
 
 module.exports = (
-	<Route name="app" path="/" handler = {Home} >
+	<Route name="app" path="/" handler = {Main} >
+		<DefaultRoute handler={Home} />
 	</Route>
 )
